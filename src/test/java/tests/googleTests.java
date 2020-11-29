20b5afde2f5124badc4d81ee0a65e4138d0d00ef
zlwqa <asdori1369@gmail.com>
2020-11-29 19:31:06 +0400

    Добавлен первый тест

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,24 +1,38 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTests {
 
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
     @Test
     public void test1() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
         driver.get("https://www.google.ru/");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER);
-        assertEquals(9,driver.findElements(By.cssSelector("#search .g")).size());
+        assertEquals(10,driver.findElements(By.cssSelector("#search .g")).size());
+    }
+
+    @AfterAll
+    public static void teardown() {
         driver.quit();
-
-
     }
 }
