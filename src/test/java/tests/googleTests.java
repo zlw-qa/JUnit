fe4515f3cae00c14ff7f560ff1192dab634639a7
zlwqa <asdori1369@gmail.com>
2020-11-29 18:44:35 +0400

    Первый сырой тест

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,8 +1,12 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTests {
 
@@ -11,6 +15,8 @@ public class googleTests {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.ru/");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER);
+        assertEquals(9,driver.findElements(By.cssSelector("#search .g")).size());
         driver.quit();
 
 
